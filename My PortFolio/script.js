09c1374e4f71326bed40b2c73ac19a02e2972dea
Githubbalaji07 <116806818+Githubbalaji07@users.noreply.github.com>
2024-04-22 15:46:20 +0530

    Add files via upload

---
+++ My PortFolio/script.js
@@ -0,0 +1,18 @@
+
+// var tablinks=document.getElementsByClassName("tab-links");
+// var tabcontents=document.getElementsByClassName("tab-contents");
+
+// function opentab(tabname){
+//     for(var tablink of tablinks){
+//         tablink.ClassList.remove("active-link");
+//     }
+
+//     for(var tabcontent of tabcontents){
+//         tabcontent.ClassList.remove("active-link");
+//     }
+
+   
+//     event(e).currentTarget.ClassList.add("active-link");
+//     document.getElementById(tabname).ClassList.add("active-link"); 
+// }
+
